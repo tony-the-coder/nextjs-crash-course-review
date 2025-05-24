9845ca0f2b3b8d0f3600215ba4ef31c461ab8a3a
Tony Aldea <tonythecoder@outlook.com>
2025-05-24 13:04:13 -0400

    Testing connection

---
+++ app/dashboard/layout.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Layout = ({ children } : {children: React.ReactNode }) =>
+{
+    return (
+        <div>
+            <h1 className="text-3xl">A Dashboard </h1>
+        </div>
+    )
+}
+export default Layout
